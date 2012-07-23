2d6463b0181386deb108f76045e558d487cd465c
Ilya Dorman <whitestar.i8@gmail.com>
2012-07-23 13:01:00 +0300

    images in mobile have no gap on top (added js file)

--- js/lightbox.js
+++ js/lightbox.js
@@ -10,7 +10,7 @@ http://lokeshdhakar.com/projects/lightbox2/
 Licensed under the Creative Commons Attribution 2.5 License - http://creativecommons.org/licenses/by/2.5/
 - free for use in both personal and commercial projects
 - attribution requires leaving author name, author link, and the license info intact
-  
+
 Thanks
 - Scott Upton(uptonic.com), Peter-Paul Koch(quirksmode.com), and Thomas Fuchs(mir.aculo.us) for ideas, libs, and snippets.
 - Artemy Tregubenko (arty.name) for cleanup and help in updating to latest proto-aculous in v2.05.
@@ -185,7 +185,11 @@ lightbox = new Lightbox options
         }
       }
       $window = $(window);
-      top = $window.scrollTop() + $window.height() / 10;
+      if (this.options.fullScreen) {
+        top = $window.scrollTop();
+      } else {
+        top = $window.scrollTop() + $window.height() / 20;
+      }
       left = $window.scrollLeft();
       $lightbox = $('.lightbox');
       $lightbox.css({
