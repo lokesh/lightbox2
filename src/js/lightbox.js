3a57d440d3a0626ab32bf32bec39d1fb2841ba78
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2017-11-25 10:37:44 -0800

    code style

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -208,9 +208,9 @@
 
     function addToAlbum($link) {
       self.album.push({
+        alt: $link.attr('data-alt'),
         link: $link.attr('href'),
-        title: $link.attr('data-title') || $link.attr('title'),
-        alt: $link.attr('data-alt')
+        title: $link.attr('data-title') || $link.attr('title')
       });
     }
 
