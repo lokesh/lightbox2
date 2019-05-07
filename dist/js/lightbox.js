63c50d2b05aee862579536d6860e1e487fd603fe
Krzysztof Chodak <krzysztof.chodak@gmail.com>
2019-05-07 20:00:56 +0200

    Update header

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -1,11 +1,11 @@
 /*!
- * Lightbox v2.10.0
+ * Lightbox v2.11.0
  * by Lokesh Dhakar
  *
  * More info:
  * http://lokeshdhakar.com/projects/lightbox2/
  *
- * Copyright 2007, 2018 Lokesh Dhakar
+ * Copyright 2007, 2018, 2019 Lokesh Dhakar
  * Released under the MIT license
  * https://github.com/lokesh/lightbox2/blob/master/LICENSE
  *
